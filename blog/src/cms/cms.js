f9177b9f2eb811661c072c25daf18aea0fbe34f5
Innocent Obi <innocentobi@nodenigeria.com>
2020-04-23 22:10:03 -0500

    testing custom widget

---
+++ blog/src/cms/cms.js
@@ -0,0 +1,7 @@
+import CMS from "netlify-cms-app";
+import {
+  CustomWidgetControl,
+  CustomWidgetPreview,
+} from "../components/customWidget";
+
+CMS.registerWidget("mywidget", CustomWidgetControl, CustomWidgetPreview);
---
+++ blog/src/components/customWidget/CustomWidgetControl.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+import CKEditor from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+export class CustomWidgetControl extends PureComponent {
+  onChange = (event, editor) => {
+    const data = editor.getData();
+    this.props.onChange(data);
+  };
+
+  render() {
+    return <CKEditor editor={ClassicEditor} onChange={this.onChange} />;
+  }
+}
