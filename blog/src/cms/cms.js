0199f75c83e6ec54331b361b27f8733e31a20367
Innocent Obi <innocentobi@nodenigeria.com>
2020-04-23 23:09:56 -0500

    config 2

--- blog/src/cms/cms.js
+++ blog/src/cms/cms.js
@@ -1,8 +1,4 @@
 import CMS from "netlify-cms-app";
 import { SlidesControl, SlidesPreview } from "./Slides";
 
-import CustomWidgetControl from "../components/customWidget/CustomWidgetControl";
-import CustomWidgetPreview from "../components/customWidget/CustomWidgetPreview";
-
-CMS.registerWidget("mywidget", CustomWidgetControl, CustomWidgetPreview);
 CMS.registerWidget("slides", SlidesControl, SlidesPreview);
