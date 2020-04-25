09546d01941dd9075748a6350163c5caccae4fcb
Innocent Obi <innocentobi@nodenigeria.com>
2020-04-25 18:51:20 -0500

    component/widget v5

--- blog/src/cms/cms.js
+++ blog/src/cms/cms.js
@@ -1,12 +1,13 @@
 import CMS from "netlify-cms-app";
-import { Widget } from "../components/widgets/iframe";
+import IframeControl from "../components/widgets/iframe/IframeControl";
+import IframePreview from "../components/widgets/iframe/IframePreview";
 
-CMS.registerWidget([Widget()]);
+CMS.registerWidget("iframe", IframeControl, IframePreview);
 
 CMS.registerEditorComponent(
   {
     id: "iframe-tag",
-    label: "Iframe",
+    label: "Iframe Tag",
     fields: [{ name: "id", label: "Embed Link", widget: "string" }],
     widget: "iframe",
     //pattern: /^iframe (\S+)$/,
