273bcbb19ef60d979c448fda80c6723ff9cba01b
Innocent Obi <innocentobi@nodenigeria.com>
2020-05-03 14:08:50 -0500

    jupyter test v1

--- blog/src/components/widgets/iframe/IframePreview.js
+++ blog/src/components/widgets/iframe/IframePreview.js
@@ -22,8 +22,8 @@ export default function IframePreview(props) {
 export function IframePreviewT({ data, id, title, allow, src }) {
   return [
     <iframe title={title} src={id} width="100%" height="375" allow={allow} />,
-    <p> {src.replace("./images/", "")}</p>,
-    <img src={src.replace("./images/", "")} />,
+    // <p> {src.replace("./images/", "")}</p>,
+    //<img src={src.replace("./images/", "")} />,
   ];
 }
 
